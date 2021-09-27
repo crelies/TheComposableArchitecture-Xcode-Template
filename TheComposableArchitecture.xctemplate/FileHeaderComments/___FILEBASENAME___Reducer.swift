6dd9e96bcbb37c4a3335c0a3a0bddb0cecbf0333
Christian Elies <celies@eos-uptrade.de>
2021-09-27 14:30:53 +0200

    refactor(): small improvements

--- TheComposableArchitecture.xctemplate/FileHeaderComments/___FILEBASENAME___Reducer.swift
+++ TheComposableArchitecture.xctemplate/FileHeaderComments/___FILEBASENAME___Reducer.swift
@@ -15,7 +15,7 @@ enum ___VARIABLE_MODULENAME___Module {}
 
 extension ___VARIABLE_MODULENAME___Module {
     static var reducer: Reducer<___VARIABLE_MODULENAME___State, ___VARIABLE_MODULENAME___Action, ___VARIABLE_MODULENAME___Environment> {
-        Reducer<___VARIABLE_MODULENAME___State, ___VARIABLE_MODULENAME___Action, ___VARIABLE_MODULENAME___Environment> { state, action, environment in
+        .init { state, action, environment in
             return .none
         }
     }
--- TheComposableArchitecture.xctemplate/FileHeaderComments/___FILEBASENAME___View.Action+feature.swift
+++ TheComposableArchitecture.xctemplate/FileHeaderComments/___FILEBASENAME___View.Action+feature.swift
@@ -12,8 +12,8 @@
 extension ___VARIABLE_MODULENAME___View.Action {
     var feature: ___VARIABLE_MODULENAME___Action {
         switch self {
-            case ___VARIABLE_MODULENAME___View.Action.onAppear:
-                return ___VARIABLE_MODULENAME___Action.onAppear
+        case .onAppear:
+            return .onAppear
         }
     }
 }
